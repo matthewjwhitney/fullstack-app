eb4aba3a97a752d636a662b667acc8b0158d1ffe
Matthew J Whitney <mwhitney@networthservices.com>
2019-01-02 13:14:20 -0700

    install redux

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,33 @@
 import React, { Component, Fragment } from "react";
 import TestForm from "./TestForm";
 import NavBar from "./NavBar";
+import Content from "./Content";
+import { connect } from "react-redux";
+import { simpleAction } from "./actions/simpleAction";
 
 class App extends Component {
+  simpleAction = event => {
+    this.props.simpleAction();
+  };
   render() {
     return (
       <Fragment>
         <NavBar />
-        <TestForm />
+        <button onClick={this.simpleAction}>Test redux action</button>
+        <Content />
       </Fragment>
     );
   }
 }
+const mapStateToProps = state => ({
+  ...state
+});
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
